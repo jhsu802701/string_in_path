d16b79eff07a1780e4551bbef3aa94b84b1bd95b
Jason Hsu <jhsu@Jasons-MacBook-Air.local>
2014-09-24 17:45:08 -0500

    Desired functions now work and pass tests

--- lib/string_in_path.rb
+++ lib/string_in_path.rb
@@ -2,13 +2,26 @@ require 'string_in_path/version'
 
 module StringInPath
   def self.present (str_to_find, path)
-    require 'find'
     require 'string_in_file'
-    Find.find(path) do |file|
-      if StringInFile.present(str_to_find,file) == true
+    require 'find'
+    Find.find(path) do |filename|
+      next if filename == ".DS_Store"
+      if StringInFile.present(str_to_find,filename) == true
         return true
       end
     end
     return false
+  end
+  
+  def self.replace (str1, str2, path)
+    require 'string_in_file'
+    require 'find'
+    Find.find(path) do |filename|
+      file_type = `file -b #{filename}`
+      if file_type.include?("directory") == false && filename.include?(".DS_Store") == false
+        StringInFile.replace(str1, str2, filename)
+        system("rm #{filename}-e")
+      end
+    end
   end
 end
--- spec/lib/string_in_path_spec.rb
+++ spec/lib/string_in_path_spec.rb
@@ -10,42 +10,79 @@ describe StringInPath do
   dir2 = "#{dir_parent}/dir2"
   dir2a = "#{dir_parent}/dir2/dir2a"
   dir2b = "#{dir_parent}/dir2/dir2b"
-  system("mkdir #{dir_parent}")
-  system("mkdir #{dir1}")
-  system("mkdir #{dir1a}")
-  system("mkdir #{dir1b}")
-  system("mkdir #{dir2}")
-  system("mkdir #{dir2a}")
-  system("mkdir #{dir2b}")
-  StringInFile.write("Chuck Norris can divide by zero.", "#{dir1}/file1.txt")
-  StringInFile.write("Chuck Norris can make Grumpy Cat happy.", "#{dir1a}/file1a.txt")
-  StringInFile.write("Chuck Norris counted to infinity - twice.", "#{dir1b}/file1b.txt")
-  StringInFile.write("Chuck Norris can start a fire by rubbing two ice cubes together.", "#{dir2}/file2.txt")
-  StringInFile.write("Waldo is hiding from Chuck Norris.", "#{dir2a}/file2a.txt")
-  StringInFile.write("Chuck Norris can win a game of Connect 4 in only 3 moves.", "#{dir2b}/file2b.txt")
+  it "should write text to files" do
+    system("mkdir -p #{dir_parent}")
+    system("mkdir -p #{dir1}")
+    system("mkdir -p #{dir1a}")
+    system("mkdir -p #{dir1b}")
+    system("mkdir -p #{dir2}")
+    system("mkdir -p #{dir2a}")
+    system("mkdir -p #{dir2b}")
+    StringInFile.write("Chuck Norris can divide by zero.", "#{dir1}/file1.txt")
+    StringInFile.write("Chuck Norris can make Grumpy Cat happy.", "#{dir1a}/file1a.txt")
+    StringInFile.write("Chuck Norris counted to infinity - twice.", "#{dir1b}/file1b.txt")
+    StringInFile.write("Chuck Norris can start a fire by rubbing two ice cubes together.", "#{dir2}/file2.txt")
+    StringInFile.write("Waldo is hiding from Chuck Norris.", "#{dir2a}/file2a.txt")
+    StringInFile.write("Chuck Norris can win a game of Connect 4 in only 3 moves.", "#{dir2b}/file2b.txt")
+  end
   
   it "should determine whether a substring is in any of the files in a directory" do
-    expect(StringInFile.read("#{dir1}/file1.txt")).to eq("Chuck Norris can divide by zero.")
-    expect(StringInFile.read("#{dir1a}/file1a.txt")).to eq("Chuck Norris can make Grumpy Cat happy.")
-    expect(StringInFile.read("#{dir1b}/file1b.txt")).to eq("Chuck Norris counted to infinity - twice.")
-    expect(StringInFile.read("#{dir2}/file2.txt")).to eq("Chuck Norris can start a fire by rubbing two ice cubes together.")
-    expect(StringInFile.read("#{dir2a}/file2a.txt")).to eq("Waldo is hiding from Chuck Norris.")
-    expect(StringInFile.read("#{dir2b}/file2b.txt")).to eq("Chuck Norris can win a game of Connect 4 in only 3 moves.")
+    expect(StringInPath.present("divide by zero", "#{dir1}")).to eq(true)
+    expect(StringInPath.present("divide by zero", "#{dir1a}")).to eq(false)
+    expect(StringInPath.present("divide by zero", "#{dir1b}")).to eq(false)
+    expect(StringInPath.present("divide by zero", "#{dir2}")).to eq(false)
+    expect(StringInPath.present("divide by zero", "#{dir2a}")).to eq(false)
+    expect(StringInPath.present("divide by zero", "#{dir2b}")).to eq(false)
+    
+    expect(StringInPath.present("Grumpy Cat", "#{dir1}")).to eq(true)
+    expect(StringInPath.present("Grumpy Cat", "#{dir1a}")).to eq(true)
+    expect(StringInPath.present("Grumpy Cat", "#{dir1b}")).to eq(false)
+    expect(StringInPath.present("Grumpy Cat", "#{dir2}")).to eq(false)
+    expect(StringInPath.present("Grumpy Cat", "#{dir2a}")).to eq(false)
+    expect(StringInPath.present("Grumpy Cat", "#{dir2b}")).to eq(false)   
+    
+    expect(StringInPath.present("counted to infinity", "#{dir1}")).to eq(true)
+    expect(StringInPath.present("counted to infinity", "#{dir1a}")).to eq(false)
+    expect(StringInPath.present("counted to infinity", "#{dir1b}")).to eq(true)
+    expect(StringInPath.present("counted to infinity", "#{dir2}")).to eq(false)
+    expect(StringInPath.present("counted to infinity", "#{dir2a}")).to eq(false)
+    expect(StringInPath.present("counted to infinity", "#{dir2b}")).to eq(false)
+    
+    expect(StringInPath.present("ice cubes", "#{dir1}")).to eq(false)
+    expect(StringInPath.present("ice cubes", "#{dir1a}")).to eq(false)
+    expect(StringInPath.present("ice cubes", "#{dir1b}")).to eq(false)
+    expect(StringInPath.present("ice cubes", "#{dir2}")).to eq(true)
+    expect(StringInPath.present("ice cubes", "#{dir2a}")).to eq(false)
+    expect(StringInPath.present("ice cubes", "#{dir2b}")).to eq(false)
+    
+    expect(StringInPath.present("Waldo", "#{dir1}")).to eq(false)
+    expect(StringInPath.present("Waldo", "#{dir1a}")).to eq(false)
+    expect(StringInPath.present("Waldo", "#{dir1b}")).to eq(false)
+    expect(StringInPath.present("Waldo", "#{dir2}")).to eq(true)
+    expect(StringInPath.present("Waldo", "#{dir2a}")).to eq(true)
+    expect(StringInPath.present("Waldo", "#{dir2b}")).to eq(false)
+    
+    expect(StringInPath.present("Connect 4", "#{dir1}")).to eq(false)
+    expect(StringInPath.present("Connect 4", "#{dir1a}")).to eq(false)
+    expect(StringInPath.present("Connect 4", "#{dir1b}")).to eq(false)
+    expect(StringInPath.present("Connect 4", "#{dir2}")).to eq(true)
+    expect(StringInPath.present("Connect 4", "#{dir2a}")).to eq(false)
+    expect(StringInPath.present("Connect 4", "#{dir2b}")).to eq(true)
   end
   
-  it "should confirm that a substring is present within a directory" do
+  it "should replace a substring in every file in a directory with a different substring" do
+    StringInPath.replace("Chuck Norris", "Shelby Marx", "#{dir1}")
+    StringInPath.replace("Chuck Norris", "Shelby Marx", "#{dir2}")
   end
   
-  it "should replace a substring with another substring for every file in a directory" do
-  end
-  
-  it "should determine whether the new substring has replaced the old one in all of the files in a directory" do
+  it "should verify that the process of replacing every substring in every file in a directory succeeded" do
     expect(StringInFile.read("#{dir1}/file1.txt")).to eq("Shelby Marx can divide by zero.")
     expect(StringInFile.read("#{dir1a}/file1a.txt")).to eq("Shelby Marx can make Grumpy Cat happy.")
     expect(StringInFile.read("#{dir1b}/file1b.txt")).to eq("Shelby Marx counted to infinity - twice.")
     expect(StringInFile.read("#{dir2}/file2.txt")).to eq("Shelby Marx can start a fire by rubbing two ice cubes together.")
     expect(StringInFile.read("#{dir2a}/file2a.txt")).to eq("Waldo is hiding from Shelby Marx.")
     expect(StringInFile.read("#{dir2b}/file2b.txt")).to eq("Shelby Marx can win a game of Connect 4 in only 3 moves.")
+    system("rm -r #{dir_parent}")
   end
-  #system("rm -r #{dir_parent}")
+
 end
